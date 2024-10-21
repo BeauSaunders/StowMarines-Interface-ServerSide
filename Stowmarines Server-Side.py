d19ec96e649a15cbfe401a85fa8d87715e2727c0
Fenn Saunders <fennjsaunders@gmail.com>
2024-10-21 22:24:59 +0100

    tmp push

--- Stowmarines Server-Side.py
+++ Stowmarines Server-Side.py
@@ -1,7 +1,6 @@
 
 
 
-
 from pathlib import Path
 import os
 import JSONFunctions
@@ -25,6 +24,20 @@ with open(smi_ss_cfg_Locator,mode= "r") as Locators:
 
 
 jsonData = JSONFunctions.LoadJSONFromFile(JSON_Location)
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+
 
 
 def system():
@@ -62,6 +75,7 @@ def system():
     def calculateModSize(Modname):
         '''Calculate a Given Mod's Total Size'''
         # Cocatenates Locator-Prefix with the Mod Input
+        print(f"Processing: {color.PURPLE}{color.BOLD}{Modname}{color.END}")
         mod_location = MODS_Directory + Modname
 
         ModsCheck = []
@@ -175,7 +189,7 @@ def system():
         with open(JSON_Location, mode="w") as file:
             file.write(newData)
         print(f"\n --- {Modname} Was Succesfully Updated In The JSON File --- \n")
-        print(f"UPDATED: {Modname} : {singlemodsize} \n")
+        print(f"{color.GREEN}UPDATED: {Modname}{color.END}  : {color.YELLOW}{singlemodsize}{color.END}\n")
 
         # Once all mods have updated, restarts the system
         if special == False:
